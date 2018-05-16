b98276c068248bd35e53664d61298e03054e4188
luisalfredocrespo <32372666+luisalfredocrespo@users.noreply.github.com>
2018-05-15 21:35:44 -0500

    pestaña de nosotros

--- templet/nosotros.php
+++ templet/nosotros.php
@@ -2,9 +2,13 @@
                     <div class="fullwidth-bg">
                     	<div class="parallax-spacing">
                     		<div class="container">
-                            	<h3 class="border-title">Reserva tu cita   <span>con anticipación </span></h3>
+                            	<h3 class="border-title jmy_web_div" data-page="inicio" id="hola75">
+<?php $this->pnt('hola75','Reserva tu cita con anticipación '); ?>
+</h3>
                                 <div class="aligncenter">
-                                	<a href="<?php echo RUTA_ACTUAL; ?>contacto" class="appointment-btn btn-eff2">Reserva tu  <span>cita</span></a>
+                                	<a href="<?php echo RUTA_ACTUAL; ?>contacto" class="appointment-btn btn-eff2 jmy_web_div" data-page="inicio" id="hola76">
+<?php $this->pnt('hola76','Reserva tu cita con anticipación '); ?>
+</a>
                               	</div>
                             </div>
                         </div>
@@ -13,15 +17,12 @@
   </div>
 
 <div class="container"><!-- Container -->
-               <h2 class="border-title aligncenter">
-                    Salon Evolution
-                 </h2>
-                 <font color=black><h4 class="aligncenter">Abre sus puertas 
-el 10 de agosto 2002 en calle 11 # 47 colonia Espartaco Coyoacán y 
-inicia con el nombre de estética capricornio el 22 de octubre cambia de 
-dirección avenida 4 # 87 colonia Espartaco Coyoacán el cual a estado 
-trabajando de la mano con los productos ALFA PARF (ITALIANOS) , tec 
-Italy (ilaliano) , salerm (español) el cual nos accesoramos  </h4></font>
+               <h2 class="border-title aligncenter jmy_web_div" data-page="inicio" id="hola77">
+<?php $this->pnt('hola77','Salon Evolution'); ?>
+</h2>
+                 <font color=black><h4 class="aligncenter jmy_web_div" data-page="inicio" id="hola78">
+<?php $this->pnt('hola78','Abre sus puertas el 10 de agosto 2002 en calle 11 # 47 colonia Espartaco Coyoacán y inicia con el nombre de estética capricornio el 22 de octubre cambia de dirección avenida 4 # 87 colonia Espartaco Coyoacán el cual a estado trabajando de la mano con los productos ALFA PARF (ITALIANOS) , tec Italy (ilaliano) , salerm (español) el cual nos accesoramos '); ?>
+</h4></font>
          <div class="clear"></div>
  </div> 				
 <div class="portfolio dt-sc-one-fourth column no-space all-sorte">
